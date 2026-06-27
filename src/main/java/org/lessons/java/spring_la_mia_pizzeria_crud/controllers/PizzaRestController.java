3cf525faae625c8e1de31704868dee318ccfe86f
Pat desktop <dmtpatricia@gmail.com>
2026-06-27 02:08:51 +0200

    PIzza Controller base version

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
@@ -0,0 +1,50 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
+
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.lessons.java.spring_la_mia_pizzeria_crud.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaRestController {
+
+    @Autowired
+    private PizzaService pizzaService;
+
+    // INDEX-------------------------------
+    @GetMapping
+    public List<Pizza> index() {
+        return pizzaService.findAll();
+    }
+    // SHOW--------------------------------
+    @GetMapping("/{id}")
+    public Pizza show(@PathVariable Integer id) {
+        return pizzaService.getById(id);
+    }
+    // STORE------------------------------
+    @PostMapping
+    public Pizza store(@RequestBody Pizza pizza) {
+        return pizzaService.create(pizza);
+    }
+    // UPDATE-----------------------------
+    @PutMapping("/{id}")
+    public Pizza update(@RequestBody Pizza pizza, @PathVariable Integer id) {
+        pizza.setId(id);
+        return pizzaService.update(pizza);
+    }
+    // DELETE----------------------------
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        pizzaService.deleteById(id);
+    }
+}
